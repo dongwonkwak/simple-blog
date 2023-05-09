5df48f6d05428e383415140f5818fc84deef063a
dongwon.kwak <dongwon.kwak@gmail.com>
2023-05-10 01:27:47 +0900

    Post 수정 기능 개발

--- src/main/java/com/dongwon/simpleblog/controller/PostController.java
+++ src/main/java/com/dongwon/simpleblog/controller/PostController.java
@@ -2,7 +2,6 @@ package com.dongwon.simpleblog.controller;
 
 import com.dongwon.simpleblog.dto.PostDto;
 import com.dongwon.simpleblog.exception.SimpleBlogException;
-import com.dongwon.simpleblog.mapper.PostMapper;
 import com.dongwon.simpleblog.service.PostService;
 import com.dongwon.simpleblog.service.UserService;
 import jakarta.validation.Valid;
@@ -21,7 +20,6 @@ import org.springframework.web.servlet.ModelAndView;
 public class PostController {
     private final PostService postService;
     private final UserService userService;
-    private final PostMapper postMapper;
 
     @GetMapping("/{id}")
     public String getPost(@PathVariable Long id, Model model) {
@@ -66,10 +64,11 @@ public class PostController {
                            Model model) {
         var optionalPost = postService.findById(id);
         if (optionalPost.isPresent()) {
-
+            model.addAttribute("post", optionalPost.get());
+            return "postForm";
         }
 
-        return "editPostForm";
+        return "404";
     }
 
     @PostMapping("/{id}/delete")
@@ -84,59 +83,6 @@ public class PostController {
         return "404";
     }
 
-
-    /*@GetMapping("/posts/{id}")
-    public String getPost(@PathVariable Long id, Model model) {
-        Optional<Post> optionalPost = postService.findById(id);
-
-        if (optionalPost.isPresent()) {
-            Post post = optionalPost.get();
-            model.addAttribute("post", post);
-            return "/post";
-        } else {
-            return "/404";
-        }
-    }
-
-    @GetMapping("/postNew")
-    public String createNewPost(Model model) {
-        String email = "homer@springfield.com";
-        var optionalUser = userService.findByEmail(email);
-
-        if (optionalUser.isPresent()) {
-            Post post = new Post();
-            post.setUser(optionalUser.get());
-            model.addAttribute("post", post);
-            return "/postForm";
-        } else {
-            return "/404";
-        }
-    }
-
-    @PostMapping("/postNew")
-    public String createNewPost(@Valid Post post,
-                                BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            return "/postForm";
-        }
-        postService.save(post);
-        return "redirect:/home";
-    }
-
-    @GetMapping("/editPost/{id}")
-    public String editPost(@PathVariable Long id,
-                           Model model) {
-        var optionalPost = postService.findById(id);
-
-        if (optionalPost.isPresent()) {
-            Post post = optionalPost.get();
-
-            model.addAttribute("post", post);
-            return "/postForm";
-        }
-
-        return "/404";
-    }*/
     @ExceptionHandler(SimpleBlogException.class)
     public ModelAndView handleSimpleBlogException(SimpleBlogException ex) {
         ModelAndView model = new ModelAndView("error");

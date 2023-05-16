f21894a3fe699ccddfbff4caa9cc89f341e1a356
dongwon.kwak <dongwon.kwak@gmail.com>
2023-05-16 10:15:59 +0900

    EnableJpaAuditing 어노테이션 JpaAuditingConfig으로 이동

--- src/main/java/com/dongwon/simpleblog/SimpleBlogApplication.java
+++ src/main/java/com/dongwon/simpleblog/SimpleBlogApplication.java
@@ -3,10 +3,8 @@ package com.dongwon.simpleblog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class SimpleBlogApplication {
 

6283dc2bba3620e4810f438ac2214d695cfe2f27
dongwon.kwak <dongwon.kwak@gmail.com>
2023-05-16 21:13:21 +0900

    초기 테스트 데이터 멀티라인으로 변경

--- src/main/java/com/dongwon/simpleblog/config/SeedData.java
+++ src/main/java/com/dongwon/simpleblog/config/SeedData.java
@@ -40,20 +40,42 @@ public class SeedData implements CommandLineRunner {
                     "password");
             userService.save(user1);
             userService.save(user2);
+            String body1 = """
+                    끝이 어딜까.
+                    너의 잠재력
+                    """;
 
             PostDto post1 = PostDto.builder()
                             .title("잠재력")
-                            .body("끝이 어딜까. \n너의 잠재력")
+                            .body(body1)
                             .build();
 
             PostDto post11 = PostDto.builder()
                             .title("123")
-                            .body("fffff")
+                            .body("""
+                                    fffff
+                                    aaa
+                                    """)
                             .build();
 
             PostDto post2 = PostDto.builder()
                             .title("너")
-                            .body("너라는 단어가 참 특별한 거 같아.\n\n 그냥 평점했던 내 하루에\n갑작스레 나타난\n너라는 사람말이야\n\n 늘 기쁘고 행복하고 활발한\n사람일 줄만 알았어\n자세히 들여다보니\n생각보다 깊더라고 사람이\n\n매일 나한테 알람 같아\n시간이 되면 너에게서\n연락이 와있거든")
+                            .body("""
+                            너라는 단어가 참 특별한 거 같아.
+                            
+                            그냥 평점했던 내 하루에
+                            갑작스레 나타난
+                            너라는 사람말이야
+                            
+                            늘 기쁘고 행복하고 활발한
+                            사람일 줄만 알았어
+                            자세히 들여다보니
+                            생각보다 깊더라고 사람이
+                            
+                            매일 나한테 알람 같아
+                            시간이 되면 너에게서
+                            연락이 와있거든
+                            """)
                             .build();
 
             postService.create(post1, user1.username());

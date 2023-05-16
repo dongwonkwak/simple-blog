8773903eecfb785c37cdf50c534326956ebc3c2d
dongwon.kwak <dongwon.kwak@gmail.com>
2023-05-16 18:48:29 +0900

    NotNull -> NotBlank로 변경

--- src/main/java/com/dongwon/simpleblog/dto/UserDto.java
+++ src/main/java/com/dongwon/simpleblog/dto/UserDto.java
@@ -2,16 +2,13 @@ package com.dongwon.simpleblog.dto;
 
 import com.dongwon.simpleblog.validation.EmailAlreadyExists;
 import com.dongwon.simpleblog.validation.UsernameAlreadyExits;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 
 @UsernameAlreadyExits
 @EmailAlreadyExists
 public record UserDto(
-        @NotNull
+        @NotBlank
         @Size(min = 4, max = 10, message = "Username must be minimum 4 characters, and maximum 10 characters long")
         String username,
         @NotNull
@@ -20,7 +17,7 @@ public record UserDto(
                 flags = Pattern.Flag.CASE_INSENSITIVE,
                 message = "Invalid Email")
         String email,
-        @NotNull
+        @NotBlank
         @Size(min = 4, max = 10, message = "Password must be minimum 9 characters, and maximum 32 characters long")
         String password) {
 }

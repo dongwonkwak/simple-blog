a7304a908008f871ca54e2d90fec12137e72d936
dongwon.kwak <dongwon.kwak@gmail.com>
2023-05-18 13:44:17 +0900

    GlobalExceptionHandler를 util 패키지로 이동

--- src/main/java/com/dongwon/simpleblog/util/GlobalExceptionHandler.java
+++ src/main/java/com/dongwon/simpleblog/util/GlobalExceptionHandler.java
@@ -1,4 +1,4 @@
-package com.dongwon.simpleblog.exception;
+package com.dongwon.simpleblog.util;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.ServletRequestBindingException;
